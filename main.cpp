fc83b8cc06b4361199d4f0a733b5efd681bf5c9d
adrianrodriguez33 <adrian.rodriguez33@upr.edu>
2025-09-09 16:26:53 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -2,17 +2,53 @@
 #include <cstdlib>
 using namespace std;
 
-main(){
+int main(){
 
     srand(time(0));
 
     int maximo = 100;
-    int numero;
+    
     int minimo = 0;
 
-    int numeroAleatorio = rand() % (maximo - minimo + 1) + minimo;
+    int numeroAleatorio1 = rand() % (maximo - minimo + 1) + minimo;
+    
+    int numeroAleatorio2 = rand() % (maximo - minimo + 1) + minimo;
 
-    cout << maximo << minimo << numero;
+    int numeroAleatorio3 = rand() % (maximo - minimo + 1) + minimo;
+    
+
+    cout<< "Números aleatorios generados: " << numeroAleatorio1 << "," << numeroAleatorio2 << "," << numeroAleatorio3 << endl;
+    cout<< "Orden Decendiente: ";
+
+    if (numeroAleatorio1 >= numeroAleatorio2 && numeroAleatorio2 >= numeroAleatorio3)
+
+        cout << numeroAleatorio1 << " >= " << numeroAleatorio2<< " >= " << numeroAleatorio3;
+
+    else if(numeroAleatorio1 <= numeroAleatorio2 && numeroAleatorio1 >= numeroAleatorio3)
+
+        cout<<  numeroAleatorio2 << ">= " << numeroAleatorio1 << " >= " << numeroAleatorio3;
+
+
+    else if (numeroAleatorio3 >= numeroAleatorio2 && numeroAleatorio2 >= numeroAleatorio1)
+
+        cout<< numeroAleatorio3 << " >= " << numeroAleatorio2 << " >= " << numeroAleatorio1;
+    
+    else if (numeroAleatorio2 >= numeroAleatorio3 && numeroAleatorio3 >= numeroAleatorio1)
+    
+        cout<< numeroAleatorio2 << " >= " << numeroAleatorio3 << " >= " << numeroAleatorio1;
+
+    else if (numeroAleatorio1 >= numeroAleatorio3 && numeroAleatorio3 >= numeroAleatorio2)
+
+        cout<< numeroAleatorio1 << " >= " << numeroAleatorio3 << " >= " << numeroAleatorio2;    
+    
+    else if (numeroAleatorio3 >= numeroAleatorio1 && numeroAleatorio1 >= numeroAleatorio2)
+        cout<< numeroAleatorio3 << " >= " << numeroAleatorio1 << " >= " << numeroAleatorio2;
+
+    else;
+
+    cout<< endl;
+
+return 0;
 
 
 
