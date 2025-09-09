5e481dea05871f47f231824aa141f00ae81a2477
adrianrodriguez33 <adrian.rodriguez33@upr.edu>
2025-09-09 14:40:41 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+main(){
+
+    srand(time(0));
+
+    int maximo = 100;
+    int numero;
+    int minimo = 0;
+
+    int numeroAleatorio = rand() % (maximo - minimo + 1) + minimo;
+
+    cout << maximo << minimo << numero;
+
+
+
+}
